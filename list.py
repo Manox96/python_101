8f206f7d047c90add429bb066dd64d5015777c96
Aymane Ennaqadi <aennaqad@e3r11p2.1337.ma>
2024-07-30 11:57:51 +0100

    new_one

---
+++ list.py
@@ -0,0 +1,19 @@
+my_list = list("aymane")
+my_list = ["a","y","m","a","n","e","z"]
+
+#to del
+del my_list[0]
+
+#to insert at the front
+my_list.insert(0,"A")
+#to insert at the end
+my_list.append("5")
+# get the max num
+max_char = max(my_list)
+#get index
+# print(my_list[my_list.index("z")])
+
+my_new_str = list("anavren")
+my_new_str.reverse();
+print(my_new_str[::2])
+#6:15 video mega
---
+++ tuples.py
@@ -0,0 +1,12 @@
+
+
+
+
+my_tuples = ("aymane","was","here","on","1337")
+my_num = ('a','b')
+combined = my_tuples + my_num
+# print(f"the tuples : {combined[-3:-1]}")
+
+my_str = "    aymane    "
+print(my_str.strip())
+
